ec327bcc8ef8b7b399a86004711ae9e6fb37bf16
anandazaugust <anandazaugust@outlook.com>
2025-12-05 05:04:40 +0530

    t

---
+++ tf/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.115"
+    }
+  
+  }
+}
+
+provider "azurerm" {
+  features {}
+  
+
+
+
+}
