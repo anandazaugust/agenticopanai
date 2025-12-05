095ce39832b6f33aa6804aa44a636db2079f2fa2
anandazaugust <anandazaugust@outlook.com>
2025-12-05 06:58:36 +0530

    t

--- tf/main.tf
+++ tf/main.tf
@@ -3,3 +3,8 @@ resource "azurerm_resource_group" "example" {
   name     = "agenticai"
   location = "uk south"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = var.rgname
+  location = uksouth
+}
