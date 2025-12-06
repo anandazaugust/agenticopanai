f67fc2849ec40d2c0105c3763681a9b264defbb2
anandazaugust <anandazaugust@outlook.com>
2025-12-07 05:19:05 +0530

    t

--- tf/main.tf
+++ tf/main.tf
@@ -8,3 +8,10 @@ resource "azurerm_resource_group" "examttple" {
   name     = var.rgname
   location = "uksouth"
 }
+
+module "avm-res-resources-resourcegroup_example_default-aurerm-v4" {
+  source  = "Azure/avm-res-resources-resourcegroup/azurerm//examples/default-aurerm-v4"
+  version = "0.2.1"
+  name = "avmrg"
+  location = "uksouth"
+}
