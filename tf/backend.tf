d83dbd81db2a7b2309cbc549040a2c667df71e07
anandazaugust <anandazaugust@outlook.com>
2025-12-08 06:22:02 +0530

    t

---
+++ tf/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.0"
+
+  backend "azurerm" {
+    resource_group_name  = "cloud-shell-storage-centralindia"       # match bootstrap outputs (or change to your names)
+    storage_account_name = "test11232223"  # replace or use -backend-config
+    container_name       = "tf1"
+    # key                  = "myproject.terraform.tfstate"
+    # access_key can be provided via -backend-config or environment variable
+  }
+}
+
